5db4c8afc5fad42356df59e0fa74c93176e4d250
JSGREEN1129 <jsg12212@outlook.com>
2025-12-13 18:30:40 +0000

    update settings

--- GreenSquareCapital/settings.py
+++ GreenSquareCapital/settings.py
@@ -130,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('users:dashboard')
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
